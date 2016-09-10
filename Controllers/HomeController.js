d87c72dd561b5eb39573e32dfcba780daf2d6ac5
Prabhakar Reddy <prabhasun.rv@gmail.com>
2016-09-10 18:52:53 +0530

    table creation

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -1,4 +1,10 @@
 app.controller("HomeController",['$scope','$http','$window','config',
 	function ($scope,$http,$window,config) {
 	$scope.name = "Welcome to Angular JS Samples";
-}])
+	$http.get("data/sampleJson.json").success(function(response){
+		$scope.candidate=response;
+		console.log($scope.candidate);
+	});
+
+
+}]);
